dae4eab23980955966b4ba649910ae57b1f2b344
Stew <ferp43728@gmail.com>
2025-05-03 15:56:42 -0400

    window move and resize

---
+++ js/windows.js
@@ -0,0 +1,107 @@
+const windowContainer = document.getElementById('window-container')
+const windowTemplate = document.getElementById('window-template')
+
+class Window {
+    constructor(startWidth, startHeight, start_x, start_y) {
+        this.windowNode = windowTemplate.content.cloneNode(true).children[0]
+        this.inner = this.windowNode.getElementsByClassName('inner-window')[0]
+        this.actions = this.windowNode.getElementsByClassName('window-actions')[0]
+        this.content = this.windowNode.getElementsByClassName('window-content')[0]
+        this.windowNode.style.width = startWidth + "px"
+        this.windowNode.style.height = startHeight + "px"
+        this.windowNode.style.left = start_x + "px"
+        this.windowNode.style.top = start_y + "px"
+        windowContainer.appendChild(this.windowNode)
+        makeWindowDraggable(this)
+        makeWindowResizable(this)
+    }
+}
+
+function makeWindowDraggable(targetWindow) {
+    var velocityX = 0, velocityY = 0
+    var lastPosX = 0, lastPosY = 0
+    var windowDraggable = false
+
+    targetWindow.actions.addEventListener('mousedown', function(e) {
+        lastPosX = e.clientX
+        lastPosY = e.clientY
+        windowDraggable = true
+    })
+
+    document.addEventListener('mousemove', function(e) {
+        if (windowDraggable) {
+            velocityX = lastPosX - e.clientX
+            velocityY = lastPosY - e.clientY
+            lastPosX = e.clientX
+            lastPosY = e.clientY
+
+            targetWindow.windowNode.style.left = (targetWindow.windowNode.offsetLeft - velocityX) + "px"
+            targetWindow.windowNode.style.top = (targetWindow.windowNode.offsetTop - velocityY) + "px"
+        }
+    })
+
+    document.addEventListener('mouseup', function(e) {
+        windowDraggable = false
+    })
+}
+
+function makeWindowResizable(targetWindow) {
+    const windowStyle = window.getComputedStyle(targetWindow.windowNode)
+    const valueRe = /\d+/
+    const padT = parseInt(valueRe.exec(windowStyle.getPropertyValue('padding-top'))[0])
+    const padR = parseInt(valueRe.exec(windowStyle.getPropertyValue('padding-right'))[0])
+    const padB = parseInt(valueRe.exec(windowStyle.getPropertyValue('padding-bottom'))[0])
+    const padL = parseInt(valueRe.exec(windowStyle.getPropertyValue('padding-left'))[0])
+
+    var lastPosX = 0, lastPosY = 0
+    var velocityX = 0, velocityY = 0
+    var windowResizeX = 0, windowResizeY = 0 //vector for which direction to resize ((1, 0) for right, (-1, 0) for left, (-1, 1) for bottom-left, etc.)
+    var windowResizable = false
+
+    targetWindow.inner.addEventListener('mousedown', function(e) {
+        e.stopPropagation() //makes sure to only resize when the outside of the window is clicked
+    })
+
+    targetWindow.windowNode.addEventListener('mousedown', function(e) {
+        lastPosX = e.clientX
+        lastPosY = e.clientY
+        //set windowResizeX and Y
+        if (e.offsetX < padL) {
+            windowResizeX = -1
+        }
+        if (e.offsetX > (targetWindow.windowNode.offsetWidth - padR - 1)) {
+            windowResizeX = 1
+        }        
+        if (e.offsetY < padT) {
+            windowResizeY = -1
+        }
+        if (e.offsetY > (targetWindow.windowNode.offsetHeight - padB - 1)) {
+            windowResizeY = 1
+        }
+        windowResizable = true
+    })
+
+    document.addEventListener('mousemove', function(e) {
+        if (windowResizable) {
+            velocityX = lastPosX - e.clientX
+            velocityY = lastPosY - e.clientY
+            lastPosX = e.clientX
+            lastPosY = e.clientY
+            targetWindow.windowNode.style.width = (targetWindow.windowNode.offsetWidth - padL - padR - (velocityX * windowResizeX)) + "px"
+            targetWindow.windowNode.style.height = (targetWindow.windowNode.offsetHeight - padT - padB - (velocityY * windowResizeY)) + "px"
+            if (windowResizeX === -1) {
+                targetWindow.windowNode.style.left = (targetWindow.windowNode.offsetLeft - velocityX) + "px"
+            }  
+            if (windowResizeY === -1) {
+                targetWindow.windowNode.style.top = (targetWindow.windowNode.offsetTop - velocityY) + "px"
+            }            
+        }
+    })
+
+    document.addEventListener('mouseup', function (e) {
+        windowResizeX = 0, windowResizeY = 0
+        windowResizable = false
+    })
+}
+
+var newWindow = new Window(600, 400, 200, 100)
